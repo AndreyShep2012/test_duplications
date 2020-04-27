a1d9d89677aa13034bb0d508255ccbdcd5dab1a8
Andrey Shepelenko <>
2020-04-27 13:23:20 +0300

    used sum package

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func sum(v1, v2 int) int {
-	return v1 + v2
-}
+	"github.com/AndreyShep2012/test_duplications/sum"
+)
 
 func main() {
 	v1 := 5
 	v2 := 10
-	fmt.Println("sum of", v1, "and", v2, "is", sum(v1, v2))
+	fmt.Println("sum of", v1, "and", v2, "is", sum.Sum(v1, v2))
 }

cc0efbf713ed1881d9f01535aece71ccf52e780d
Andrey Shepelenko <>
2020-04-27 13:45:18 +0300

    more duplications

--- proj2/main.go
+++ proj2/main.go
@@ -11,5 +11,11 @@ func sum(v1, v2 int) int {
 func main() {
 	v1 := 5
 	v2 := 10
+
+	test1 := "hello"
+	test2 := "hello"
+
 	fmt.Println("sum of", v1, "and", v2, "is", sum(v1, v2))
+
+	fmt.Println(test1, test2)
 }

8f45dc2136e3ca604ab8d17482417a8d59a9b249
Andrey Shepelenko <>
2020-06-15 13:51:37 +0300

    asdsad

--- add-repo-service/main.go
+++ add-repo-service/main.go
@@ -60,6 +60,7 @@ func main() {
 
 	rrrrr2(1, 2, 3, 4, 5, 6, 7, 8)
 	rrrrr(1, 2, 3, 4, 5, 6, 7, 8)
+	rrrrr3(1, 2, 3, 4, 5, 6, 7, 8)
 
 	service := api.NewService(serviceCfg)
 	startService(service)
@@ -70,6 +71,10 @@ func rrrrr2(p1, p2, p3, p4, p5, p6, p7, p8 int) int {
 }
 
 func rrrrr(p1, p2, p3, p4, p5, p6, p7, p8 int) int {
+	return 1
+}
+
+func rrrrr3(p1, p2, p3, p4, p5, p6, p7, p8 int) int {
 	return 1
 }
 
